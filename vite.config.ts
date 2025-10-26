673b606a87a5290dcfe66f0d433c083b442b28f5
VenkatMadasu31 <edu202834@gmail.com>
2025-10-26 12:19:13 +0530

    Auto-update 2025-10-26 12:19:13

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command, mode }) => {
-  return {
-    plugins: [react()],
-    server: {
-      port: 5173,
-      proxy: {
-        // proxy /digilocker/* to your Mock Digilocker server
-        "/digilocker": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/digilocker/, ""), // maps /digilocker/consent/verify -> /consent/verify
-        },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      "/digilocker": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/digilocker/, ""),
       },
     },
-  };
+  },
 });
