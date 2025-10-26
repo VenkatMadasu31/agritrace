1cb2c4fcfda44df3544aec231e9d650f2da69a4a
VenkatMadasu31 <edu202834@gmail.com>
2025-10-26 12:14:17 +0530

    Auto-update 2025-10-26 12:14:17

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import "./firebaseConfig";
+import DigilockerTest from "./pages/DigilockerTest";
 
 function App() {
   const [count, setCount] = useState(0)
